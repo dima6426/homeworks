906902122ceddbca825afe3344d0d7084ab86b09
Dmitry_Pichugin <picugind65@gmail.com>
2025-09-30 14:47:31 +0300

    Homework

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,22 +1,45 @@
-(function(){
-    document.addEventListener('click', burgerInit )
+(function () {
+    document.addEventListener('click', burgerInit)
 
-    function burgerInit (e){
-        const burgerIcon  = e.target.closest('.burger-icon')
+    function burgerInit(e) {
+        const burgerIcon = e.target.closest('.burger-icon')
         const burgerNavLink = e.target.closest('.nav__link')
 
-        if(!burgerIcon  && !burgerNavLink) return
+        if (!burgerIcon && !burgerNavLink) return
 
-        if(documentElement.clientWidth > 900) return
+        if (documentElement.clientWidth > 900) return
 
-        if(!document.body.classList.contains('body--opened-menu')){
+        if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu')
         }
-        else{
+        else {
             document.body.classList.remove('body--opened-menu')
         }
 
     }
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+
 
 
 })
