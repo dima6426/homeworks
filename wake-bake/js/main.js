e59f67d7e0e3f109c2c61760a07e04435401c3e7
Dmitry_Pichugin <picugind65@gmail.com>
2025-10-04 14:22:48 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -170,5 +170,13 @@
     });
 
 
+    // -------------------
+
+
+    const telInputs = document.querySelectorAll('input[type="tell"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask = (telInputs)
+
+
 
 })()
